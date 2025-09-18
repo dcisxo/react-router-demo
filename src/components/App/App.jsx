a3d84a1203907647fcdc8150a16a33caa7a82cb4
Damian <dcamp090799@gmail.com>
2025-09-17 22:28:37 -0400

    Sprint 11: Practice
    feat: implement routing and navigation with react-router-dom

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,27 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "../Dashboard/Dashboard";
 import Header from "../Header/Header";
+import Reviews from "../Reviews/Reviews";
+import AboutMe from "../AboutMe/AboutMe";
+import AboutUs from "../AboutUs/AboutUs";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+      <Routes>
+        <Route path="/reviews" element={<Reviews />} />
+      </Routes>
+      <Routes>
+        <Route path="/about-me" element={<AboutMe />} />
+      </Routes>
+      <Routes>
+        <Route path="/about-us" element={<AboutUs />} />
+      </Routes>
     </div>
   );
 }
